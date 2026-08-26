206b6aa16865f8cd88485158ef976b8743307065
TIRTHPATEL3086 <tirthp6355@gmail.com>
2026-08-26 13:14:04 +0530

    chore: remove .broker-venv and yolov8n.pt from git, update .gitignore

--- .broker-venv/Lib/site-packages/amqtt/contrib/shadows/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Module for the shadow state plugin."""
-
-from .plugin import ShadowPlugin, ShadowTopicAuthPlugin
-from .states import ShadowOperation
-
-__all__ = ["ShadowOperation", "ShadowPlugin", "ShadowTopicAuthPlugin"]
